684acc07160dc42899fa2c03f85412d210fbfe12
ekulac <kulacedanur460@gmail.com>
2025-02-05 17:44:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ProductTest();
+
+
+
+
+
+
+        }
+
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+          
+
+        }
+
+
+
+
+    }
+}
+
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,80 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId = 1, CategoryId = 1, ProductName="Bardak", UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId = 2, CategoryId = 1, ProductName="Kamera", UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId = 3, CategoryId = 2, ProductName="Telefon", UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId = 4, CategoryId = 2, ProductName="Klavye", UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId = 5, CategoryId = 2, ProductName="Fare", UnitPrice=85,UnitsInStock=1},
+            };
+        }
+
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //Ben bu "_products.Remove(product);" kodu yazdığımda neden eleman silemem? 
+            /// Cevap: referans tip olduğu için silemeyiz.
+            // Product productToDelete = new Product(); ----> bu çok hatalı bir kod gereksiz yere referans kodunu aklımızda tuttuk
+            //Product productToDelete = null;
+
+            //foreach (var item in _products)
+            //{
+            //    if (item.ProductId == product.ProductId)
+            //    {
+            //        productToDelete = item;
+            //    }
+            //}
+            //  productToDelete = _products.SingleOrDefault(p =>);  bu kod forech işlemini yapar
+            // Lamba
+            /// Lınq Language Intergrate Query
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId); // Tek bir ürün bulmaya yarar
+
+            _products.Remove(productToDelete);
+        }
+
+        //veri tabanındaki datayı business katmanına vermem lazım.
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+            //Where şunu yapar; içindeki şarta uyan tüm elemanları yeni bir listeye ekler ve geri döndürür.
+        }
+
+        public void Update(Product product)
+        {
+
+            Product productToUpdate = null;
+
+            //Gönderdiğim ürün Idsine sahip olan listedeki ürünü bul
+            productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.CategoryId = product.CategoryId;
+
+        }
+    }
+}
